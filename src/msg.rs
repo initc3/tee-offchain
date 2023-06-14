04d1467f041938e52f7bd3e2e2e6ee06702a1734
Gabriel Arrouye <33171826+Riderfighter@users.noreply.github.com>
2023-06-14 15:08:02 -0400

    All the stuff, WEEEEE

---
+++ src/msg.rs
@@ -0,0 +1,36 @@
+use cosmwasm_schema::cw_serde;
+use cosmwasm_std::{Binary, Uint128};
+
+#[cw_serde]
+pub struct InstantiateMsg {}
+
+#[cw_serde]
+pub enum ExecuteMsg {
+    IterateHash {
+        counter: Uint128,
+        current_hash: Binary,
+        old_mac: Binary,
+    },
+    ApplyUpdate {
+        new_counter: Uint128,
+        new_hash: Binary,
+    }
+}
+
+/// QueryMsg that the contract exposes
+#[cw_serde]
+pub enum QueryMsg {
+    GetState {},
+}
+
+/// queries using permits instead of viewing keys
+#[cw_serde]
+pub enum QueryWithPermit {
+    Permissioned {},
+}
+
+#[cw_serde]
+pub enum QueryAnswer {
+    QueryExAns {},
+    ViewingKeyError { error: String },
+}
