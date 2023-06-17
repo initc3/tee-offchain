5d678f6d852ea84022dc8505de492ee1e8b15ffa
Amber <136363751+aseira@users.noreply.github.com>
2023-06-17 14:40:38 -0700

    updated keplr wallet img

--- kit/test/apps/dev-only/src/routes/+page.svelte
+++ kit/test/apps/dev-only/src/routes/+page.svelte
@@ -2,17 +2,22 @@
 	import { base, assets } from '$app/paths';
 </script>
 
-<div>
-<img src="" alt="Swirl colors top graphic" width="100" height="100" loading="lazy">
+<body>
+
+<div id="swirl">
+<img src="/static/swirl-top.svg" alt="Swirl colors top graphic" width="100" height="100" loading="lazy">
+</img>
 </div>
 
 <div class="centered">
 <h1>TEE contract offchain demo</h1>
 
-<h2>Start</h2>
+<h2>Connect wallet</h2>
 <p>From:</p> <!--filled in by wallet-->
 <p>Balance:</p> <!--filled in by wallet-->
-<p>To begin using Secret Network, connect your Keplr wallet.</p>
+<p>To begin using Secret Network, connect your Keplr wallet.</p> <br>
+<a href=""><img src="/static/keplr_logo.png" alt="Keplr logo" width="64" height="64">
+</img> </a>
 
 <div id="content">
 <h2>Send</h2>
@@ -44,24 +49,39 @@
 <!--POST Receipt () Seq number, from, amount, memo-->
 </div>
 
+</body>
 
 <style>
 
-.bcknd {
+body {
+ background-image: url("../static/cover.png");
+ background-color: #333;
+}
+
+
+#swirl {
+  	opacity: 0.8;
+  	z-index: 1;
+	position: absolute;
+	max-width: 40em;
+
+}
 
 .centered {
-		max-width: 40em;
+		opacity: 0.2;
+  		z-index: 2;
+		max-width: 30em;
 		margin: 2em auto; 
-    position: relative;
+    	position: relative;
 	}
 
 	#content{
-        border: .5em solid #333;
+        opacity: 0.4;
+		position: relative;
+		border: .5em solid #333;
         background-color: #333366;
         margin: 2em auto;
         padding: 1em;
-        position: relative;
-        opacity: 30%;
         
     }
     
