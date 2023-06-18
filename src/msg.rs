3e9305757030852489f5b27b016d3a35a477ed25
Sylvain Bellemare <sbellem@gmail.com>
2023-06-17 23:38:47 -0400

    Revert "Loser party"

--- src/msg.rs
+++ src/msg.rs
@@ -1,4 +1,4 @@
-use cosmwasm_schema::{cw_serde, QueryResponses};
+use cosmwasm_schema::cw_serde;
 use cosmwasm_std::{Binary, Uint128};
 
 #[cw_serde]
@@ -15,13 +15,10 @@ pub enum ExecuteMsg {
 
 /// QueryMsg that the contract exposes
 #[cw_serde]
-#[derive(QueryResponses)]
 pub enum QueryMsg {
     /// Grab the state for a querier
-    #[returns(GetStateResp)]
     GetState {},
     /// Iterate upwards bc cool
-    #[returns(IterateHashResp)]
     IterateHash {
         counter: Uint128,
         current_hash: Binary,
@@ -30,14 +27,14 @@ pub enum QueryMsg {
 }
 
 #[cw_serde]
-pub struct GetStateResp {
+pub struct GetStateAnswer {
     pub counter: Uint128,
     pub current_hash: Binary,
     pub current_mac: Binary,
 }
 
 #[cw_serde]
-pub struct IterateHashResp {
+pub struct IterateHashAnswer {
     pub new_counter: Uint128,
     pub new_hash: Binary,
     pub new_mac: Binary
